a23aedbb212e8ca81a315f32f46f204ea1413e53
lckisaac <81701487+lckisaac@users.noreply.github.com>
2023-03-23 18:51:10 +0900

    Update keymap.c
    enabled auto mouse layer except layer 3

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
@@ -50,12 +50,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
                   RESET    , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , RESET
   ),
+  
+   [4] = LAYOUT_universal(
+    _______  ,  _______   , _______    , _______   , _______    , _______    ,                             _______  , _______  , _______    , _______    , _______   , _______   ,
+    _______  ,  _______ , KC_BTN2  , KC_BTN3   , KC_BTN1   , _______   ,                                     KC_BTN4  , KC_BTN1  , KC_BTN3    , KC_BTN2  , _______  , _______  ,
+    _______  ,  _______ , _______  , _______ , _______  , _______  ,                                         KC_BTN5  , _______  , _______  , _______  , _______  , _______  ,
+                  _______  , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+  ),
 };
 // clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 3);
+  switch(get_highest_layer(remove_auto_mouse_layer(state, true))) {
+        case 3:
+            // Auto enable scroll mode when the highest layer is 3
+            // remove_auto_mouse_target must be called to adjust state *before* setting enable
+            state = remove_auto_mouse_layer(state, false);
+            set_auto_mouse_enable(false);
+            keyball_set_scroll_mode(true);
+            break;
+        default:
+            set_auto_mouse_enable(true);
+            keyball_set_scroll_mode(false);
+            break;
+    }
+
     return state;
 }
 
@@ -68,3 +87,8 @@ void oledkit_render_info_user(void) {
     keyball_oled_render_ballinfo();
 }
 #endif
+
+void pointing_device_init_user(void) {
+    // set_auto_mouse_layer(<mouse_layer>); // only required if AUTO_MOUSE_DEFAULT_LAYER is not set to index of <mouse_layer>
+    set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
+}
