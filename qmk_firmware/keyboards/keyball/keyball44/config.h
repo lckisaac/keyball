14837a18da2df34980ef3d9bc0f8100a545e0fa4
lckisaac <81701487+lckisaac@users.noreply.github.com>
2023-04-17 12:15:43 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball44/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/config.h
@@ -85,4 +85,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Automatic Mouse Layer
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 4
-#define TAPPING_TERM 150
+#define TAPPING_TERM 200
