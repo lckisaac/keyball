4c6afa49bd66b0eaa73f79dba1a2f09a3943ec20
lckisaac <81701487+lckisaac@users.noreply.github.com>
2023-04-18 23:13:27 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
@@ -24,37 +24,37 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
-    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT1  ,
-              KC_LALT,KC_LGUI,LCTL_T(KC_LNG2)     ,LT(1,KC_SPC),LT(3,KC_LNG2),                  KC_BSPC,LT(2,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , KC_PSCR
+    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS   ,
+    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
+    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_BSLS  ,
+              KC_LCTL,KC_LGUI,ALT_T(KC_LNG2)     ,LT(1,KC_SPC),LT(3,KC_LNG1),                  KC_BSPC,LT(2,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , MO(1)
   ),
 
   [1] = LAYOUT_universal(
-    _______  ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                         KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    _______  ,  _______ , _______  , KC_UP   , KC_ENT   , KC_DEL   ,                                         KC_PGUP  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , KC_F12   ,
-    _______  ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
-                  _______  , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+    KC_GRV  ,  KC_1   , KC_2    , KC_3   , KC_4    , KC_5    ,                                         KC_6    , KC_7    , KC_8    , KC_9    , KC_0   , KC_MINS   ,
+    _______  ,  _______ , KC_UP  , KC_EQL   , KC_LBRC   , KC_RBRC   ,                                         KC_F1  , KC_F2  , KC_F3    , KC_F4  , KC_F5  , KC_F6   ,
+    _______  ,  KC_LEFT , KC_DOWN  , KC_RGHT , _______  , _______  ,                                         KC_F7  , KC_F8  , KC_F9  , KC_F10  , KC_F11  , KC_F12  ,
+                  _______  , _______ , _______  ,         _______  , _______  ,                   KC_DEL  , _______  , _______       , _______  , _______
   ),
 
   [2] = LAYOUT_universal(
-    _______  ,S(KC_QUOT), KC_7     , KC_8    , KC_9     , S(KC_8)  ,                                         S(KC_9)  , S(KC_1)  , S(KC_6)  , KC_LBRC  , S(KC_4)  , _______  ,
-    _______  ,S(KC_SCLN), KC_4     , KC_5    , KC_6     , KC_RBRC  ,                                         KC_NUHS  , KC_MINS  , S(KC_EQL), S(KC_3)  , KC_QUOT  , S(KC_2)  ,
-    _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_RBRC),                                        S(KC_NUHS),S(KC_INT1), KC_EQL   ,S(KC_LBRC),S(KC_SLSH),S(KC_INT3),
-                  KC_0     , KC_DOT  , _______  ,         _______  , _______  ,                   KC_DEL   , _______  , _______       , _______  , _______
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                         KC_PSLS  , KC_7     , KC_8     , KC_9     , KC_0    , KC_PMNS  ,
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                         KC_PAST  , KC_4     , KC_5     , KC_6     , KC_EQL  , KC_BSPC  ,
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                         KC_0     , KC_1     , KC_2     , KC_3     , KC_PDOT  , KC_RSFT  ,
+                  _______  , _______  , _______  ,         _______  , _______  ,                   _______   , _______  , _______       , _______  , KC_ENT
   ),
 
   [3] = LAYOUT_universal(
-    RGB_TOG  , _______  , _______  , _______  , _______  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
-                  _______    , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , _______
+    RGB_TOG  , RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW  , _______  ,                                        _______  , _______  , _______  , _______  , _______  , KBC_SAVE ,
+    RGB_HUI  , RGB_SAI  , RGB_VAI  , RGB_VAI  ,KC_KB_VOLUME_UP, KC_VOLU ,                                        KC_BTN4  , KC_BTN1  , KC_BTN3  , KC_BTN2 , _______  , SCRL_DVI  ,
+    RGB_HUD , RGB_SAD  , RGB_VAD  , RGB_VAD  ,KC_KB_VOLUME_DOWN, KC_VOLD,                                        KC_BTN5  , KC_LEFT  , KC_DOWN  , KC_UP   , KC_RGHT  , SCRL_DVD ,
+                  KC_KB_MUTE, KC_MUTE  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
   ),
   
    [4] = LAYOUT_universal(
     _______  ,  _______   , _______    , _______   , _______    , _______    ,                             _______  , _______  , _______    , _______    , _______   , _______   ,
-    _______  ,  _______ , KC_BTN2  , KC_BTN3   , KC_BTN1   , _______   ,                                     KC_BTN4  , KC_BTN1  , KC_BTN3    , KC_BTN2  , _______  , _______  ,
-    _______  ,  _______ , _______  , _______ , _______  , _______  ,                                         KC_BTN5  , _______  , _______  , _______  , _______  , _______  ,
+    _______  ,  _______ , KC_BTN2  , KC_BTN3   , KC_BTN1   , CPI_I100   ,                                     KC_BTN4  , KC_BTN1  , KC_BTN3    , KC_BTN2  , SCRL_MO  , CPI_I1K  ,
+    _______  ,  _______ , _______  , _______ , _______  , CPI_D100  ,                                         KC_BTN5  , _______  , _______  , _______  , _______  , CPI_D1K  ,
                   _______  , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
   ),
 };
